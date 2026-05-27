8b82233f5c15fd0deb9fe7f3ee967af5649ccd1c
fongxiammi <xiammi@outlook.com>
2026-05-27 15:02:19 +0800

    处理静态路由匹配bug

--- Backend/src/apps/Question/urls.py
+++ Backend/src/apps/Question/urls.py
@@ -1,4 +1,4 @@
-﻿from django.urls import path
+from django.urls import path
 
 from .views import QuestionBaseView
 from .views import ErrorArchiveView
@@ -8,11 +8,11 @@ from .views import AgentSelectQuestionsView
 
 urlpatterns = [
     path('questions', QuestionBaseView.as_view(), name='question-list-create'),
+    path('questions/paper-available', QuestionsPaperView.as_view(), name='question-paper-available'),
+    path('questions/agent-select', AgentSelectQuestionsView.as_view(), name='question-agent-select'),
     path('questions/<str:id>', QuestionBaseView.as_view(), name='question-detail'),
     path('error-archives', ErrorArchiveView.as_view(), name='error-archive-list-create'),
     path('error-archives/<str:id>', ErrorArchiveView.as_view(), name='error-archive-detail'),
-    path('questions/paper-available', QuestionsPaperView.as_view(), name='question-paper-available'),
-    path('questions/agent-select', AgentSelectQuestionsView.as_view(), name='question-agent-select'),
 ]
 
 
